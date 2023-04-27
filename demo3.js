49a9196896ec70348cc5a4d8eda320f35e2efbfc
MR GAURISHANKAR KHADGA <130385414+gaurishankarkhadga@users.noreply.github.com>
2023-04-28 00:45:53 +0530

    Add files via upload

---
+++ demo3.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+prompt("hello GAURISHNKAR ");
+for(i=0;i<5;i++)
+{
+    prompt("jai shree ram");
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+function clicked(){
+    console.log("this is clicked");
+
+}
+window.onload=function(){
+console.log("onload your window")
+}
+
+Firstcontainer.addEventListener('mouse',function(){
+    console.log("container click ho gyaa")
+})
+
+Firstcontainer.addEventListener('click',function(){
+    console.log("container click ho gyaa")
+})
+
+Firstcontainer.addEventListener('click',function(){
+    console.log("container click ho gyaa")
+})
